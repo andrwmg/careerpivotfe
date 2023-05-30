55d0f8210592156364337b6e05e11b7c8409bfd6
andrwmg <andrwmg@gmail.com>
2023-05-30 14:15:28 -0700

    Bug fixes

--- src/pages/Post/PostCard.js
+++ src/pages/Post/PostCard.js
@@ -8,6 +8,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { GlobalContext } from "../../contexts/GlobalContext";
 import formatCount from "../../utils/formatCount";
 import formatDate from "../../utils/formatDate";
+import { motion } from 'framer-motion'
 
 export default function PostCard({ post, showComments, commentCount }) {
 
@@ -89,64 +90,92 @@ export default function PostCard({ post, showComments, commentCount }) {
     }, [post])
 
     return (
-        <Card sx={{ width: '100%', minHeight: '237px', bgcolor: 'rgba(232, 235, 255, 0)', color: 'black', p: 3, borderRadius: 2, border: '1px solid #E8EBFF', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)', display: 'flex', flexDirection: 'column', alignItems: 'start', gap: 3, flexGrow: 1 }}>
+        <Card sx={{ width: '100%', minHeight: '237px', bgcolor: 'rgba(232, 235, 255, 0)', color: 'black', p: 3, borderRadius: 2, border: '1px solid #E8EBFF', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)', display: 'flex', flexDirection: 'column', alignItems: 'start', gap: 3, flexGrow: 1}}>
             {/* <Grid container item direction='column' alignItems='start' xs={12} rowGap={3} height='fit-content' maxWidth='100%'> */}
-            <Grid container item gap={1} width='100%'>
-                <Grid container item direction='column' xs={12}>
-                <Typography variant="h3" fontWeight={700} lineHeight='35px' textAlign='start'>
-                    {post ? post.title : <Skeleton />}
-                </Typography>
-                </Grid>
-                <Grid container item direction='column' xs={12}>
+            {post ?
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: .5 }} style={{ flexGrow: 1, height: '100%' }}>
+                    <Grid container item direction='column' gap={3}>
+                        <Grid container item gap={1} flexGrow={1}>
+                            <Grid container item direction='column' xs={12}>
+                                <Typography variant="h3" fontWeight={700} lineHeight='35px' textAlign='start'>
+                                    {post.title}
+                                </Typography>
+                            </Grid>
+                            <Grid container item direction='column' xs={12}>
 
-                <Typography variant='subtitle1' color='text.secondary' lineHeight='20px' textAlign='start'>
-                    {post ?
-                        <span>
-                            {`${formatDate(post.createdAt)} by `}
-                            <i>
-                                {post.author.username}
-                            </i>
-                            {!location.pathname.includes('/group') && post.group ?
-                            <>
-                            {' in '}
-                            <span style={{ fontWeight: 500 }}>
-                                {`${!location.pathname.includes('/group') && post.group ? post.group.title : ""}`}
-                            </span>
-                            </> : null}
-                        </span>
-                        : <Skeleton />}
-                </Typography>
-                </Grid>
-                <Grid container item direction='column' xs={12}>
+                                <Typography variant='subtitle1' color='text.secondary' lineHeight='20px' textAlign='start'>
+                                    <span>
+                                        {`${formatDate(post.createdAt)} by `}
+                                        <i>
+                                            {post.author.username}
+                                        </i>
+                                        {!location.pathname.includes('/group') && post.group ?
+                                            <>
+                                                {' in '}
+                                                <span style={{ fontWeight: 500 }}>
+                                                    {`${!location.pathname.includes('/group') && post.group ? post.group.title : ""}`}
+                                                </span>
+                                            </> : null}
+                                    </span>
+                                </Typography>
+                            </Grid>
+                            <Grid container item direction='column' xs={12}>
 
-                <Typography variant='body1' textAlign='start' lineHeight='30px' letterSpacing='-2%' minHeight='60px'>
-                    {post ? post.body : <Skeleton />}
-                </Typography>
-                </Grid>
-            </Grid>
-            <Grid container item justifyContent='start' alignItems='center' gap={4} color='primary'>
-                <Grid container item direction='column' xs='auto'>
-                    <Grid container item alignItems='center'>
-                        <IconButton onClick={like} size='small'>
-                            {status === 'liked' ?
-                                <Favorite color='primary' sx={{ fontSize: '22px' }} />
-                                :
-                                <FavoriteBorderOutlined color='primary' sx={{ fontSize: '22px' }} />
-                            }
-                        </IconButton>
-                        <Typography variant='h4' color='black' minWidth='9px'>{post ? formatCount(likeCount) : <Skeleton />}</Typography>
+                                <Typography variant='body1' textAlign='start' lineHeight='30px' letterSpacing='-2%' minHeight='60px'>
+                                    {post.body}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container item justifyContent='start' alignItems='center' gap={4} color='primary'>
+                            <Grid container item direction='column' xs='auto'>
+                                <Grid container item alignItems='center'>
+                                    <IconButton onClick={like} size='small'>
+                                        {status === 'liked' ?
+                                            <Favorite color='primary' sx={{ fontSize: '22px' }} />
+                                            :
+                                            <FavoriteBorderOutlined color='primary' sx={{ fontSize: '22px' }} />
+                                        }
+                                    </IconButton>
+                                    <Typography variant='h4' color='black' minWidth='9px'>{formatCount(likeCount)}</Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid container item direction='column' xs>
+                                <Grid container item xs='auto' alignItems='center'>
+                                    <IconButton onClick={showComments} size='small'>
+                                        <CommentOutlined color="primary" sx={{ fontSize: '22px' }} />
+                                    </IconButton>
+                                    <Typography variant='h4' color='black' textAlign='start' minWidth='9px'>{formatCount(commentCount)}</Typography>
+                                </Grid>
+                            </Grid>
+
+                        </Grid>
                     </Grid>
-                </Grid>
-                <Grid container item direction='column' xs>
-                    <Grid container item xs='auto' alignItems='center'>
-                        <IconButton onClick={showComments} size='small'>
-                            <CommentOutlined color="primary" sx={{ fontSize: '22px' }} />
-                        </IconButton>
-                        <Typography variant='h4' color='black' textAlign='start' minWidth='9px'>{post ? formatCount(commentCount) : <Skeleton />}</Typography>
-                    </Grid>
-                </Grid>
+                </motion.div>
+                :
+                <Grid container item direction='column' gap={3}>
+                    <Grid container item gap={1} flexGrow={1}>
+                        <Grid container item direction='column' xs={12}>
+                            <Typography variant="h3" lineHeight='35px'>
+                                <Skeleton />
+                            </Typography>
+                        </Grid>
+                        <Grid container item direction='column' xs={12}>
 
-            </Grid>
+                            <Typography variant='subtitle1' color='text.secondary' lineHeight='20px' textAlign='start'>
+                                <Skeleton />
+                            </Typography>
+                        </Grid>
+                        <Grid container item direction='column' xs={12}>
+
+                            <Typography variant='body1' textAlign='start' lineHeight='30px' letterSpacing='-2%' minHeight='60px'>
+                                <Skeleton />
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Typography fontSize='22px'>
+                        <Skeleton sx={{ width: '125px' }} />
+                    </Typography>
+                </Grid>}
         </Card>
     )
 }
