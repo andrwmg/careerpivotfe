b045f5ff19bd6d89e485b043ab744a6246916559
andrwmg <andrwmg@gmail.com>
2023-06-01 10:24:35 -0700

    Add group pages, implement new post UI, bug fixes

--- src/pages/Groups/GroupsPage.js
+++ src/pages/Groups/GroupsPage.js
@@ -54,12 +54,12 @@ export default function GroupsPage() {
 
     return (
         <Grid container item gap={1} maxWidth={{ xs: '100%', md: 'calc(100vw - 256px)' }} zIndex={0} mt={4}>
-            <Grid container item direction='column' xs={12} minHeight='195px' rowGap={1}>
+            {/* <Grid container item direction='column' xs={12} minHeight='195px' rowGap={1}>
                 <Typography variant='h4' fontWeight={700} px={{ xs: 3, md: 6 }}>
                     {`Trending ${isAuthenticated() && auth().career ? `in ${auth().career.title}` : 'now'}`}
                 </Typography>
                 <SmallCardRow groups={trending} />
-            </Grid>
+            </Grid> */}
             {/* <Feed posts={latest} loading={feedLoading} heading={isAuthenticated() ? 'Your Feed' : 'Feed'} /> */}
         </Grid>
     )
